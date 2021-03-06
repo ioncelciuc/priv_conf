b83535f28065a5c46a21125e2447e9697252c091
ioncelciuc <ioncelciuc2001@gmail.com>
2021-03-06 23:32:53 +0200

    Add Inrto Screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:zoom_clone/screens/home_page.dart';
+import 'package:zoom_clone/screens/intro_auth_screen.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(),
-  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: NavigationPage(),
+    );
+  }
+}
+
+class NavigationPage extends StatefulWidget {
+  @override
+  _NavigationPageState createState() => _NavigationPageState();
+}
+
+class _NavigationPageState extends State<NavigationPage> {
+  bool isSigned = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: isSigned == false ? IntroAuthScreen() : HomePage(),
+    );
+  }
 }
---
+++ lib/screens/intro_auth_screen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+import 'package:zoom_clone/variables.dart';
+
+class IntroAuthScreen extends StatefulWidget {
+  @override
+  _IntroAuthScreenState createState() => _IntroAuthScreenState();
+}
+
+class _IntroAuthScreenState extends State<IntroAuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return IntroductionScreen(
+      pages: [
+        PageViewModel(
+          title: 'Welcome',
+          body: 'Welcome to PriveConf, the best private video conference app!',
+          image: Center(
+            child: Image.asset(
+              'images/welcome.png',
+              height: 175,
+            ),
+          ),
+          decoration: PageDecoration(
+            bodyTextStyle: myTextStyle(20, Colors.black),
+            titleTextStyle: myTextStyle(20, Colors.black),
+          ),
+        ),
+        PageViewModel(
+          title: 'Easy to use interface',
+          body: 'Join or start meetings now! It only takes a few seconds!',
+          image: Center(
+            child: Image.asset(
+              'images/conference.png',
+              height: 175,
+            ),
+          ),
+          decoration: PageDecoration(
+            bodyTextStyle: myTextStyle(20, Colors.black),
+            titleTextStyle: myTextStyle(20, Colors.black),
+          ),
+        ),
+        PageViewModel(
+          title: 'Secure and private',
+          body:
+              'Your security is important. This app uses the JitsiMeet API, an open-source, secure and reliable service!',
+          image: Center(
+            child: Image.asset(
+              'images/secure.jpg',
+              height: 200,
+            ),
+          ),
+          decoration: PageDecoration(
+            bodyTextStyle: myTextStyle(20, Colors.black),
+            titleTextStyle: myTextStyle(20, Colors.black),
+          ),
+        ),
+      ],
+      onDone: () {
+        print('done');
+      },
+      onSkip: () {
+        print('skip');
+      },
+      showSkipButton: true,
+      skip: const Icon(Icons.skip_next, size: 45),
+      next: const Icon(Icons.arrow_forward_ios),
+      done: Text(
+        'Done',
+        style: myTextStyle(20, Colors.black),
+      ),
+    );
+  }
+}
